29b4117bfa25fbb86609a6464cb3c5e36e9102a4
Adam Balogh <abalogh@palantir.com>
2019-03-20 18:15:54 +0000

    format

--- internal/extender/overhead.go
+++ internal/extender/overhead.go
@@ -16,7 +16,6 @@ package extender
 
 import (
 	"context"
-	"k8s.io/apimachinery/pkg/api/resource"
 	"sort"
 	"sync"
 	"time"
@@ -26,6 +25,7 @@ import (
 	"github.com/palantir/witchcraft-go-logging/wlog/svclog/svc1log"
 	"github.com/palantir/witchcraft-go-logging/wlog/wapp"
 	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/labels"
 	corelisters "k8s.io/client-go/listers/core/v1"
 )
