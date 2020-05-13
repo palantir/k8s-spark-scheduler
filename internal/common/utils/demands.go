82dafde50b0cbb55784a10fccc4ae4763b36df07
Onur Satici <onursatici@users.noreply.github.com>
2020-05-13 16:20:13 +0100

    count demand creations separate from demand fulfilled events (#123)
    * count demand creations separate from demand fulfilled events
    
    * fmt

--- internal/common/utils/demands.go
+++ internal/common/utils/demands.go
@@ -19,9 +19,19 @@ import (
 	"strings"
 
 	"github.com/palantir/k8s-spark-scheduler-lib/pkg/apis/scaler/v1alpha1"
+	"github.com/palantir/k8s-spark-scheduler/internal/common"
 	"github.com/palantir/witchcraft-go-logging/wlog/svclog/svc1log"
 	v1 "k8s.io/api/core/v1"
 )
+
+// IsSparkSchedulerDemand returns whether the passed object is a demand created by the spark scheduler extender
+func IsSparkSchedulerDemand(obj interface{}) bool {
+	if demand, ok := obj.(*v1alpha1.Demand); ok {
+		_, labelFound := demand.Labels[common.SparkAppIDLabel]
+		return labelFound
+	}
+	return false
+}
 
 // OnDemandFulfilled returns a function that calls the wrapped function if the demand object is fulfilled
 func OnDemandFulfilled(ctx context.Context, fn func(*v1alpha1.Demand)) func(interface{}, interface{}) {
