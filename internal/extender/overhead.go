61c6315a3e252b2450c691c5306f835f428cb061
Ramzi Karam <rkaram@users.noreply.github.com>
2020-08-17 16:05:45 +0100

    PR comments

--- internal/extender/overhead.go
+++ internal/extender/overhead.go
@@ -23,16 +23,10 @@ import (
 	"github.com/palantir/k8s-spark-scheduler/internal/common/utils"
 	"github.com/palantir/witchcraft-go-logging/wlog/svclog/svc1log"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/types"
 	coreinformers "k8s.io/client-go/informers/core/v1"
 	corelisters "k8s.io/client-go/listers/core/v1"
 	clientcache "k8s.io/client-go/tools/cache"
-)
-
-var (
-	oneCPU = resource.NewMilliQuantity(1000, resource.DecimalSI)
-	oneGiB = resource.NewQuantity(1*1024*1024*1024, resource.BinarySI)
 )
 
 // OverheadComputer computes non spark scheduler managed pods total resources periodically
@@ -142,16 +136,15 @@ func (o *OverheadComputer) computeNodeOverhead(ctx context.Context, nodeName str
 		}
 		if !o.resourceReservationManager.PodHasReservation(ctx, pod) {
 			overhead.Add(podRequestInfo.requests)
-			if pod.Spec.SchedulerName != common.SparkSchedulerName {
+			if pod.Spec.SchedulerName == common.SparkSchedulerName {
+				if _, appHasResourceReservation := o.resourceReservationManager.GetResourceReservation(pod.Labels[common.SparkAppIDLabel], pod.Namespace); appHasResourceReservation {
+					svc1log.FromContext(ctx).Warn("found spark scheduler pod with no reservation but application has a resource reservation",
+						svc1log.SafeParam("nodeName", nodeName),
+						svc1log.SafeParam("podName", podRequestInfo.podName),
+						svc1log.SafeParam("podNamespace", podRequestInfo.podNamespace))
+				}
+			} else {
 				nonSchedulableOverhead.Add(podRequestInfo.requests)
-			}
-
-			if podRequestInfo.requests.CPU.AsDec().Cmp(oneCPU.AsDec()) > 0 || podRequestInfo.requests.Memory.AsDec().Cmp(oneGiB.AsDec()) > 0 {
-				svc1log.FromContext(ctx).Debug("Container with no resource reservation has high resource requests",
-					svc1log.SafeParam("podName", pod.Name),
-					svc1log.SafeParam("nodeName", pod.Spec.NodeName),
-					svc1log.SafeParam("CPU", podRequestInfo.requests.CPU),
-					svc1log.SafeParam("Memory", podRequestInfo.requests.Memory))
 			}
 		}
 	}
@@ -198,29 +191,10 @@ func (o *OverheadComputer) deletePodRequests(obj interface{}) {
 	}
 }
 
-func (o *OverheadComputer) addPodResourcesToGroupResources(ctx context.Context, groupResources map[string]resources.NodeGroupResources, pod *v1.Pod, instanceGroup string) {
-	if _, ok := groupResources[instanceGroup]; !ok {
-		groupResources[instanceGroup] = resources.NodeGroupResources{}
-	}
-	currentOverhead := groupResources[instanceGroup]
-	if _, ok := currentOverhead[pod.Spec.NodeName]; !ok {
-		currentOverhead[pod.Spec.NodeName] = resources.Zero()
-	}
-	currentOverhead[pod.Spec.NodeName].Add(podToResources(ctx, pod))
-}
-
 func podToResources(ctx context.Context, pod *v1.Pod) *resources.Resources {
 	res := resources.Zero()
 	for _, c := range pod.Spec.Containers {
 		resourceRequests := c.Resources.Requests
-		if resourceRequests.Cpu().AsDec().Cmp(oneCPU.AsDec()) > 0 || resourceRequests.Memory().AsDec().Cmp(oneGiB.AsDec()) > 0 {
-			// TODO(rkaram): Move this away from here
-			svc1log.FromContext(ctx).Debug("Container with no resource reservation has high resource requests",
-				svc1log.SafeParam("podName", pod.Name),
-				svc1log.SafeParam("nodeName", pod.Spec.NodeName),
-				svc1log.SafeParam("CPU", resourceRequests.Cpu()),
-				svc1log.SafeParam("Memory", resourceRequests.Memory()))
-		}
 		res.AddFromResourceList(resourceRequests)
 	}
 	return res
