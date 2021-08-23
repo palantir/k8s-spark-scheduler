30bcc524b0a9dc1fc3e7963a804b5686f03df82a
Chris Battarbee <cbattarbee@palantir.com>
2021-08-23 11:42:12 +0100

    eval $(minikube docker-env) so we build the docker image locally for minikube and it doesnt pull

--- hack/dev/run-in-minikube.sh
+++ hack/dev/run-in-minikube.sh
@@ -9,7 +9,7 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/../..
 
-#eval $(minikube docker-env)
+eval $(minikube docker-env)
 $SCRIPT_ROOT/godelw docker build --verbose
 
 kubectl apply -f $SCRIPT_ROOT/examples/extender.yml
