8ca2df2163b5d62ab4acf17ffd20f79c67cd322d
Chris Battarbee <cbattarbee@palantir.com>
2021-09-23 11:39:36 +0100

    Add annotations

--- examples/submit-test-spark-app.sh
+++ examples/submit-test-spark-app.sh
@@ -9,8 +9,10 @@ APP_ID=$1
 EXECUTOR_COUNT=$2
 DRIVER_CPU=$3
 DRIVER_MEM=$4
-EXECUTOR_CPU=$5
-EXECUTOR_MEM=$6
+DRIVER_NVIDIA_GPUS=$5
+EXECUTOR_CPU=$6
+EXECUTOR_MEM=$7
+EXECUTOR_NVIDIA_GPUS=$8
 
 # create driver
 kubectl create -f <(cat << EOF
@@ -24,8 +26,10 @@ metadata:
   annotations:
     spark-driver-cpu: "$DRIVER_CPU"
     spark-driver-mem: "$DRIVER_MEM"
+    spark-driver-nvidia.com/gpu: "$DRIVER_NVIDIA_GPUS"
     spark-executor-cpu: "$EXECUTOR_CPU"
     spark-executor-mem: "$EXECUTOR_MEM"
+    spark-executor-nvidia.com/gpu: "$EXECUTOR_NVIDIA_GPUS"
     spark-executor-count: "$EXECUTOR_COUNT"
 spec:
   schedulerName: spark-scheduler
@@ -38,6 +42,7 @@ spec:
       requests:
         cpu: "$DRIVER_CPU"
         memory: "$DRIVER_MEM"
+        nvidia.com/gpu: "$DRIVER_NVIDIA_GPUS"
 EOF)
 
 # wait for driver to be running
@@ -72,6 +77,7 @@ spec:
       requests:
         cpu: "$EXECUTOR_CPU"
         memory: "$EXECUTOR_MEM"
+        nvidia.com/gpu: "$EXECUTOR_NVIDIA_GPUS"
 EOF)
 done
 
