3bd7efd81087f3945a30da602de4fc251aa9e12a
Chris Battarbee <cbattarbee@palantir.com>
2021-09-27 18:47:55 +0100

    Generate and mount certs through config maps

--- hack/dev/run-in-minikube.sh
+++ hack/dev/run-in-minikube.sh
@@ -9,7 +9,13 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/../..
 
+$SCRIPT_ROOT/hack/dev/generate-certs.sh
+
 #eval $(minikube docker-env)
 $SCRIPT_ROOT/godelw docker build --verbose
+
+kubectl apply -f $SCRIPT_ROOT/examples/namespace.yml
+
+kubectl create configmap scheduler-secrets --namespace=spark --from-file="${SCRIPT_ROOT}/out/tmp/generated_keys/"
 
 kubectl apply -f $SCRIPT_ROOT/examples/extender.yml
