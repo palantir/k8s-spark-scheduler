945b1f6bf7e7cab5baa5bda7e46a432d20b2e23e
Chris Battarbee <cbattarbee@palantir.com>
2021-12-01 13:49:03 +0000

    Run locally

--- hack/dev/run-in-minikube.sh
+++ hack/dev/run-in-minikube.sh
@@ -17,5 +17,6 @@ $SCRIPT_ROOT/godelw docker build --verbose
 kubectl apply -f $SCRIPT_ROOT/examples/namespace.yml
 
 kubectl create configmap scheduler-secrets --namespace=spark --from-file="${SCRIPT_ROOT}/out/tmp/generated_keys/"
+kubectl create configmap resource-reservation-conversion-webhook-secrets --namespace=spark --from-file="${SCRIPT_ROOT}/out/tmp/generated_keys/"
 
 kubectl apply -f $SCRIPT_ROOT/examples/extender.yml
