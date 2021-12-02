d8687d685c7647a90cfffecfbeed97ff53b9d235
Chris Battarbee <cbattarbee@palantir.com>
2021-12-02 13:22:34 +0000

    Remove conversions webhook from scheduler

--- internal/conversionwebhook/resource_reservation.go
+++ internal/conversionwebhook/resource_reservation.go
@@ -23,10 +23,8 @@ import (
 	werror "github.com/palantir/witchcraft-go-error"
 	"github.com/palantir/witchcraft-go-logging/wlog/svclog/svc1log"
 	"github.com/palantir/witchcraft-go-server/config"
-	"github.com/palantir/witchcraft-go-server/wrouter"
 	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/webhook/conversion"
 )
 
 const (
@@ -41,26 +39,19 @@ func init() {
 	_ = sparkscheme.AddToScheme(scheme)
 }
 
-// InitializeCRDConversionWebhook initialized conversion webhook and returns webhook client
-// configuration pointing to the webhook.
-func InitializeCRDConversionWebhook(
+// GetWebhookClientConfig returns webhook client configuration pointing to the webhook.
+func GetWebhookClientConfig(
 	ctx context.Context,
-	router wrouter.Router,
 	server config.Server,
 	schedulerNamespace string,
 	schedulerServiceName string,
 	schedulerServicePort int32,
 	schedulerServiceContextPath string,
 ) (*v1.WebhookClientConfig, error) {
-	err := addConversionWebhookRoute(ctx, router)
-	if err != nil {
-		return nil, err
-	}
-
 	path := filepath.Join(schedulerServiceContextPath, webhookPath)
 
 	if len(server.ClientCAFiles) == 0 {
-		return nil, werror.WrapWithContextParams(ctx, err, "No client CA bundle provided, can not generate conversion webhook client config")
+		return nil, werror.ErrorWithContextParams(ctx, "No client CA bundle provided, can not generate conversion webhook client config")
 	}
 
 	if len(server.ClientCAFiles) > 1 {
@@ -82,18 +73,4 @@ func InitializeCRDConversionWebhook(
 		},
 		CABundle: caBundle,
 	}, nil
-}
-
-// addConversionWebhookRoute adds resource reservation crd version conversion webhook
-func addConversionWebhookRoute(ctx context.Context, router wrouter.Router) error {
-	svc1log.FromContext(ctx).Info("Initializing resource reservation crd conversion webhook")
-	webhook := conversion.Webhook{}
-	err := webhook.InjectScheme(scheme)
-	if err != nil {
-		return werror.WrapWithContextParams(ctx, err, "failed to inject scheme into conversion webhook")
-	}
-	if err := router.Post(webhookPath, &webhook); err != nil {
-		return werror.WrapWithContextParams(ctx, err, "failed to add /convert route")
-	}
-	return nil
 }
