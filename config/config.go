561b586350155a3b5cb3927998e54740a3519e54
Chris Battarbee <cbattarbee@palantir.com>
2021-12-06 17:12:18 +0000

    Remove redundant config

--- config/config.go
+++ config/config.go
@@ -50,7 +50,6 @@ type WebhookServiceConfig struct {
 	Namespace   string `yaml:"namespace"`
 	ServiceName string `yaml:"service-name"`
 	ServicePort int32  `yaml:"service-port"`
-	ContextPath string `yaml:"context-path"`
 }
 
 // FifoConfig enables the fine-tuning of FIFO enforcement
