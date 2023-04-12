399571a580fb6c8e6d5d0954eeffa1d9a6a3409b
Kevin Simons <ksimons@palantir.com>
2023-04-12 09:54:35 -0700

    Move

--- cmd/integration/common.go
+++ cmd/integration/common.go
@@ -28,6 +28,7 @@ import (
 	"github.com/palantir/witchcraft-go-server/config"
 	"github.com/palantir/witchcraft-go-server/witchcraft"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -119,5 +120,24 @@ func waitForCondition(ctx context.Context, t *testing.T, condition func() bool) 
 				return
 			}
 		}
+	}
+}
+
+func getAffinityForInstanceGroup(labelKey string, instanceGroupName string) *corev1.Affinity {
+	return &corev1.Affinity{
+		NodeAffinity: &corev1.NodeAffinity{
+			RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+				NodeSelectorTerms: []corev1.NodeSelectorTerm{
+					{
+						MatchExpressions: []corev1.NodeSelectorRequirement{
+							{
+								Key:    labelKey,
+								Values: []string{instanceGroupName},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 }
--- cmd/integration/server_test.go
+++ cmd/integration/server_test.go
@@ -176,22 +176,7 @@ func Test_StaticCompaction(t *testing.T) {
 				},
 			},
 			Spec: v1.PodSpec{
-				Affinity: &v1.Affinity{
-					NodeAffinity: &v1.NodeAffinity{
-						RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
-							NodeSelectorTerms: []v1.NodeSelectorTerm{
-								{
-									MatchExpressions: []v1.NodeSelectorRequirement{
-										{
-											Key:    "resource_channel",
-											Values: []string{"desiredInstanceGroup"},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
+				Affinity: getAffinityForInstanceGroup("resource_channel", "desiredInstanceGroup"),
 			},
 		},
 		NodeNames: &nodeNames,
